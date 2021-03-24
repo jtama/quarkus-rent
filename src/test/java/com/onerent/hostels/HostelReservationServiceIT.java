c4f1bc6bc8bbf9cd322c4efc319171e179c9c946
Jérôme TAMA <j.tama@groupeonepoint.com>
2021-03-24 16:29:04 +0100

    intergration tests init

---
+++ src/test/java/com/onerent/hostels/HostelReservationServiceIT.java
@@ -0,0 +1,54 @@
+package com.onerent.hostels;
+
+import com.onerent.DatabaseResource;
+import com.onerent.rocket.Rocket;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.common.mapper.TypeRef;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpStatus;
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.Matchers.startsWith;
+
+@QuarkusTest
+@QuarkusTestResource(DatabaseResource.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class HostelReservationServiceIT {
+
+    @Test
+    @Order(1)
+    void testInitialItems() {
+        List<Hostel> hostels = get("/api/hostels").then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract().body().as(new TypeRef<>() {
+
+                });
+
+        List<Rocket> rockets = get("/api/rockets").then()
+                .statusCode(HttpStatus.SC_OK)
+                .header(HttpHeaders.CONTENT_TYPE, startsWith(MediaType.APPLICATION_JSON))
+                .extract().body().as(new TypeRef<>() {
+                });
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(hostels).hasSize(1)
+                    .element(0).hasFieldOrPropertyWithValue("name", "Ritz");
+            softly.assertThat(rockets).hasSize(1)
+                    .element(0).hasFieldOrPropertyWithValue("name", "APOLLO 1");
+        });
+
+    }
+
+}
